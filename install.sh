9839fd381cdf17c08b9f9bf85b6da836a97ab1c9
typoworx-de <info@typoworx.com>
2020-02-25 11:10:26 +0100

    Update install.sh
    adding flag '--no-x-check' to avoid 'You appear to be running an X server; please exit X before installing.'

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ depotPath='/usr/src/nvidia-proprietary/depot';
 depotFile=$(ls "${depotPath}/NVIDIA-Linux-${arch}"*.run | sort -nr | head -n 1)
 
 echo -e "\e[34mInstalling from proprietary package: '$(basename $depotFile)'\e[0m";
-sh ${depotFile} -q -a -n -X -s --install-libglvnd --dkms --run-nvidia-xconfig && {
+sh ${depotFile} -q -a -n -X -s --no-x-check --install-libglvnd --dkms --run-nvidia-xconfig && {
   echo -e "\e[32mDone successfully\e[0m\n";
 } || {
   echo -e "\e[31mFailed with errors!\e[0m\n";
